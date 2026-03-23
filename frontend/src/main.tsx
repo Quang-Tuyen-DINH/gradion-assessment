370ed0a25bff1d5b04c9eaf19f1a8767fc4ad676
Quang Tuyen DINH <qtuyen43@gmail.com>
2026-03-23 21:47:53 +0700

    feat(frontend): install antd + dayjs + icons, wrap app in ConfigProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ConfigProvider } from 'antd';
 import { AppRouter } from './router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppRouter />
+    <ConfigProvider theme={{ token: { colorPrimary: '#4f46e5' } }}>
+      <AppRouter />
+    </ConfigProvider>
   </React.StrictMode>,
 );
